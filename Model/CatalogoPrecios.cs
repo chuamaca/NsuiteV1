f65d70466f5ee141336cceec85a1538abd58e80e
Cesar Huamani <cesarhuamanicastro@gmail.com>
2020-06-08 01:19:11 -0500

    UltimaPresentacion

---
+++ Model/CatalogoPrecios.cs
@@ -0,0 +1,133 @@
+﻿
+
+namespace Model
+{
+    using Helper;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
+    using System.Data.Entity.Spatial;
+    using System.Data.Entity.Validation;
+    using System.IO;
+    using System.Linq;
+    using System.Web;
+
+    [Table("CatalogoPrecios")]
+    public class CatalogoPrecios
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idCatalogoPrecio { get; set; }
+
+        [DisplayName("Nombre")]
+        [StringLength(30)]
+        public string nombre { get; set; }
+
+        [DisplayName("caracteristicas")]
+        [StringLength(30)]
+        public string caracteristicas { get; set; }
+
+        [DisplayName("precio")]
+        [StringLength(30)]
+        public string precio { get; set; }
+
+        [DisplayName("otros")]
+        [StringLength(30)]
+        public string otros { get; set; }
+
+        [DisplayName("empresa")]
+        [StringLength(30)]
+        public string empresa { get; set; }
+
+        [DisplayName("estado")]
+        [StringLength(30)]
+        public string estado { get; set; }
+
+        //public JsonResult listarAlumnos()
+        //{
+        //    var lista = (bd.Alumno.Where(p => p.BHABILITADO.Equals(1))
+        //        .Select(p => new
+        //        {
+        //            p.IIDALUMNO,
+        //            p.NOMBRE,
+        //            p.APPATERNO,
+        //            p.APMATERNO,
+        //            p.TELEFONOPADRE
+        //        })).ToList();
+        //    return Json(lista, JsonRequestBehavior.AllowGet);
+        //}
+
+
+        public List<CatalogoPrecios> ListarCatalogoPorEmpresa(string nombreempresa)
+        {
+            var catalogo = new List<CatalogoPrecios>();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    catalogo = ctx.CatalogoPrecios.Where(p => p.empresa == nombreempresa).Distinct().ToList();
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+
+            return catalogo;
+        }
+
+
+
+        //para lado CLIENTE
+
+
+
+
+        public List<CatalogoPrecios> ListarCatalogoPorEmpresaTipo(string nombreempresa, string tipo)
+        {
+            var catalogo = new List<CatalogoPrecios>();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    catalogo = ctx.CatalogoPrecios.Where(p => p.empresa == nombreempresa && p.nombre==tipo).ToList();
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+
+            return catalogo;
+        }
+
+
+        public List<CatalogoPrecios> ListarCatalogoPorEmpresayTipo(string nombreempresa, string tipo, string caracteristicas)
+        {
+            var catalogo = new List<CatalogoPrecios>();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    catalogo = ctx.CatalogoPrecios.Where(p => p.empresa == nombreempresa && p.nombre ==tipo && p.caracteristicas==caracteristicas).ToList();
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+
+            return catalogo;
+        }
+
+
+
+    }
+}
---
+++ proyecto/Areas/Admin/Controllers/LiquidacionsController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace proyecto.Areas.Admin.Controllers
+{
+    public class LiquidacionsController : Controller
+    {
+        private ProyectoContext db = new ProyectoContext();
+
+
+        public Liquidacion liquidacion = new Liquidacion();
+
+        public int afectado = 0;
+
+        // GET: Admin/Liquidacions
+        public ActionResult Index()
+        {
+            return View(db.Liquidacion.ToList());
+        }
+
+        public ActionResult configuracion()
+        {
+            return View();
+        }
+
+        // GET: Admin/Liquidacions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Liquidacion liquidacion = db.Liquidacion.Find(id);
+            if (liquidacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(liquidacion);
+        }
+
+        // GET: Admin/Liquidacions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Liquidacions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idLiquidaciones,procesos,estado,contratointerno,refacturable,mes,referencia,doc,numerodocumento,c_fact,fechaemision,fechainicio,fechafin,credito,rucempresa,empresa,responsable,sucursal,ruccliente,cliente,usuariofinal,tipousuario,ordenservicio,fechaordenservicio,rqcliente,contrato,guiaremision,tipo,codigoequipo,tipoequipo,serie,marca,modelo,parnumber,bateria,cargador,procesador,velocidad,ram,disco,licencia,nombreequipo,usuariooficce,cableseguridad,mouse,maletin,softwareadicional,accesorios,observaciones,moneda,valor,igv,total,sefacturo")] Liquidacion liquidacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Liquidacion.Add(liquidacion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(liquidacion);
+        }
+
+        // GET: Admin/Liquidacions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Liquidacion liquidacion = db.Liquidacion.Find(id);
+            if (liquidacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(liquidacion);
+        }
+
+        // POST: Admin/Liquidacions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+            //PARA MODIFICAR LOS CAMPOS EDITAR **
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idLiquidaciones,procesos,estado,contratointerno,refacturable,mes,referencia,doc,numerodocumento,c_fact,fechaemision,fechainicio,fechafin,credito,rucempresa,empresa,responsable,sucursal,ruccliente,cliente,usuariofinal,tipousuario,ordenservicio,fechaordenservicio,rqcliente,contrato,guiaremision,tipo,codigoequipo,tipoequipo,serie,marca,modelo,parnumber,bateria,cargador,procesador,velocidad,ram,disco,licencia,nombreequipo,usuariooficce,cableseguridad,mouse,maletin,softwareadicional,accesorios,observaciones,moneda,valor,igv,total,sefacturo, identificador")] Liquidacion liquidacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(liquidacion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(liquidacion);
+        }
+
+        // GET: Admin/Liquidacions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Liquidacion liquidacion = db.Liquidacion.Find(id);
+            if (liquidacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(liquidacion);
+        }
+
+        // POST: Admin/Liquidacions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Liquidacion liquidacion = db.Liquidacion.Find(id);
+            db.Liquidacion.Remove(liquidacion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        public JsonResult GenerarLiquidacion(string valor)
+        {
+            var rm = new ResponseModel();
+            //HAy que validar que estos se genere una sola vez al mes
+            //Es decir que si es marzo solo tenga una vez registrada ese mes
+            RRR_Liquidacion_mensual();
+           
+
+            if (afectado != -1)
+            {
+                rm.response = true;
+                rm.message = "SE AFECTO [ " + afectado + " ]  FILAS";
+                rm.href = Url.Content("~/admin/liquidacions/index");
+                // ViewBag.rta = "LAs columnas afectadas fueron" + afectado;
+            }else
+            {
+                rm.response = false;
+                rm.message = "YA EXISTE LIQUIDACION DEL MES ACTUAL";
+                rm.href = "self";
+            }
+            return Json(rm,JsonRequestBehavior.AllowGet);
+
+          //  return Redirect("~/admin/liquidacion/index");
+
+
+        }
+
+        public void RRR_Liquidacion_mensual()
+        {
+            using (var ctx = new ProyectoContext())
+            {
+
+                afectado = ctx.Database.ExecuteSqlCommand("RRR_insert_liquidacion");
+            }
+            //var f = new ProyectoContext();
+            ////    SqlParameter param1 = new SqlParameter("@idhardware", idhardware);
+            //return f.Database.SqlQuery<Liquidacion>("RRR_insert_liquidacion").AllAsync(estado == true
+            //    );
+        }
+    }
+}
